c7a4951142a1240bca499250f26f9bbc7eb3f615
ZeoXer <andrew17413@gmail.com>
2024-04-22 17:57:59 +0800

    upload init project

---
+++ truffle/contracts/SimpleStorage.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Store {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // 賣家 => 商品編號 => 商品庫存
+    mapping(address => mapping(uint256 => uint256)) public inventory;
+    // 賣家 => 商品編號 => 商品價格
+    mapping(address => mapping(uint256 => uint256)) public prices;
+    // 賣家帳戶餘額
+    mapping(address => uint256) public balances;
+
+    // 紀錄監聽重要事件
+    event ProductAdd(
+        address indexed seller,
+        uint256 indexed productId,
+        uint256 price,
+        uint256 stock
+    );
+    event ProductUpdate(
+        address indexed seller,
+        uint256 indexed productId,
+        uint256 price,
+        uint256 stock
+    );
+    event ProductRemove(address indexed seller, uint256 indexed productId);
+    event ProductBuy(
+        address indexed buyer,
+        address indexed seller,
+        uint256 indexed productId,
+        uint256 price,
+        uint256 stock
+    );
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function addProduct(
+        uint256 productId,
+        uint256 price,
+        uint256 stock
+    ) public {
+        inventory[msg.sender][productId] = stock;
+        prices[msg.sender][productId] = price;
+        emit ProductAdd(msg.sender, productId, price, stock);
+    }
+
+    function updateProduct(
+        uint256 productId,
+        uint256 price,
+        uint256 stock
+    ) public {
+        require(inventory[msg.sender][productId] > 0, "Product not exist");
+        inventory[msg.sender][productId] = stock;
+        prices[msg.sender][productId] = price;
+        emit ProductUpdate(msg.sender, productId, price, stock);
+    }
+
+    function removeProduct(uint256 productId) public {
+        require(inventory[msg.sender][productId] > 0, "Product not exist");
+        inventory[msg.sender][productId] = 0;
+        prices[msg.sender][productId] = 0;
+        emit ProductRemove(msg.sender, productId);
+    }
+
+    function buyProduct(
+        address seller,
+        uint256 productId,
+        uint256 quantity
+    ) public payable {
+        require(
+            inventory[seller][productId] > quantity,
+            "Insufficient quantity of product"
+        );
+        require(
+            msg.value >= prices[seller][productId] * quantity,
+            "Insufficient funds"
+        );
+        balances[seller] += prices[seller][productId] * quantity;
+        inventory[seller][productId] -= quantity;
+        emit ProductBuy(
+            msg.sender,
+            seller,
+            productId,
+            prices[seller][productId],
+            inventory[seller][productId]
+        );
+    }
+
+    function withdrawBalance() public {
+        require(balances[msg.sender] > 0, "Insufficient funds");
+        uint256 amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        payable(msg.sender).transfer(amount);
+    }
+}
